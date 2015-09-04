8c21f05c77fcae9935fdff731950d3e35c5850fd
Nik Adli <adli89@live.com>
2015-09-04 22:14:09 +0800

    Cleaned layout + copied code from AudioSpectrum.

--- Integration/Integration.ino
+++ Integration/Integration.ino
@@ -12,7 +12,7 @@ Port LED code from Adafruit Picollo
 #include <Adafruit_LEDBackpack.h>
 #include <math.h>
 #include <Adafruit_GFX.h>
-#include <stdint.h>  //For New Arduino
+#include <stdint.h> //For New Arduino
 #include <fix_fft.h>
 #include <Adafruit_PWMServoDriver.h>
 #define pin_adc 0
@@ -39,11 +39,13 @@ Port LED code from Adafruit Picollo
   Adafruit_BicolorMatrix matrix = Adafruit_BicolorMatrix();
 
   static const uint8_t 
+  
   // This is low-level noise that's subtracted from each FFT output column:
   noise[64]={ 8,6,6,5,3,4,4,4,3,4,4,3,2,3,3,4,
               2,1,2,1,3,2,3,2,1,2,3,1,2,3,4,4,
               3,2,2,2,2,2,2,1,3,2,2,2,2,2,2,2,
               2,2,2,2,2,2,2,2,2,2,2,2,2,3,3,4 },
+              
   // These are scaling quotients for each FFT output column, sort of a
   // graphic EQ in reverse.  Most music is pretty heavy at the bass end.
   eq[64]={
@@ -51,6 +53,7 @@ Port LED code from Adafruit Picollo
       0,   0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
       0,   0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
       0,   0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0 },
+      
   // When filtering down to 8 columns, these tables contain indexes
   // and weightings of the FFT spectrum output values to use.  Not all
   // buckets are used -- the bottom-most and several at the top are
@@ -77,6 +80,7 @@ Port LED code from Adafruit Picollo
     143, 164, 179, 185, 184, 174, 158, 139, 118,  97,
      77,  60,  45,  34,  25,  18,  13,   9,   7,   5,
       3,   2,   2,   1,   1,   1,   1 },
+      
   // And then this points to the start of the data for each of the columns:
   * const colData[]  = {
     col0data, col1data, col2data, col3data,
@@ -93,8 +97,8 @@ Port LED code from Adafruit Picollo
   // Called this way, it uses the default address 0x40
   Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
   
-  #define SERVOMIN  100 // this is the 'minimum' pulse length count (out of 4096)
-  #define SERVOMAX  488 // this is the 'maximum' pulse length count (out of 4096)
+  #define SERVOMIN  100 // This is the 'minimum' pulse length count (out of 4096)
+  #define SERVOMAX  488 // This is the 'maximum' pulse length count (out of 4096)
   uint8_t servonum = 0;
   uint16_t pulseLength = 0;
   uint16_t pulselength0 = 0;
@@ -130,8 +134,11 @@ void setup()
     datacolumn   = (uint8_t *)&colData[i];
     nBins        = datacolumn[0] + 2;
     binNum       = datacolumn[1];
+    
     for(colDiv[i]=0, j=2; j<nBins; j++)
+    {
       colDiv[i] += datacolumn[j];
+    }
   }
 
   Serial.begin(9600);
@@ -149,5 +156,176 @@ void setup()
 
 void loop() 
 {
+  uint8_t  x, L, nBins, binNum, weighting, c;
+  uint8_t * datacolumn;
+  uint16_t minLvl, maxLvl;
+  int level, y, sum;
+  static long tt;
+  int val;
+  
+  if(debug)  Serial.println("Start sampling audio");
+    
+  i = 0;
+  while(i < 128)
+  {
+    analog_value[i] = analogRead(pin_adc);
+    i++;  
+  }
+  
+  if(debug)  Serial.println("Finish sampling audio");
 
+  for (i=0; i<128; i++)
+  { 
+    // Prepare data for FFT
+    data[i] = analog_value[i];
+    im[i] = 0;
+  }
+  
+  // This could be done with the fix_fftr function without the im array.
+  if(debug)  Serial.println("Start Fourier Transform");
+  
+  fix_fft(data, im, 7, 0);
+  
+  // I am only interested in the absolute value of the transformation
+  if(debug)  Serial.println("Get absolute value transformation");
+  
+  for (i=0; i<64; i++)
+  {
+    data[i] = sqrt(data[i] * data[i] + im[i] * im[i]);    
+    
+    // Remove noise and eq level
+    if(data[i] < noise[i])
+    {
+      data[i] = 0; 
+    }
+    else
+    {
+      data[i] = ((data[i] - noise[i])*( (256L - eq[i])) >> 4);
+    }
+  }
+  
+  delay(2);
+  
+  // Fill background w/colors, then idle parts of columns will erase
+  matrix.fillRect(0, 0, 8, 3, LED_RED);    // Upper section
+  matrix.fillRect(0, 3, 8, 2, LED_YELLOW); // Mid
+  matrix.fillRect(0, 5, 8, 3, LED_GREEN);  // Lower section
+
+  if(debug)  Serial.println("Downsample spectrum output to 8 columns");
+  
+  // Downsample spectrum output to 8 columns:  
+  for(x=0; x<8; x++) 
+  {
+    datacolumn   = (uint8_t *)&colData[x];
+    nBins  = datacolumn[0] + 2;
+    binNum = datacolumn[1];
+    
+    if(debug)  Serial.println("Weighted Average summation");
+    
+    for(sum=0, i=2; i<nBins; i++)
+    {
+      sum += (data[binNum++])*(datacolumn[i]); // Weighted
+    }
+
+    if(debug)  Serial.println("Averaging weighted average");
+    
+    col[x][colCount] = sum / colDiv[x]; // Average
+    minLvl = maxLvl = col[x][0];
+    
+    if(debug)  Serial.println("Get range of prior 10 frames");
+    
+    for(i=1; i<10; i++) 
+    { 
+      // Get range of prior 10 frames
+      if(col[x][i] < minLvl)      minLvl = col[x][i];
+      else if(col[x][i] > maxLvl) maxLvl = col[x][i];
+    }
+    
+    // minLvl and maxLvl indicate the extents of the FFT output, used
+    // for vertically scaling the output graph (so it looks interesting
+    // regardless of volume level).  If they're too close together though
+    // (e.g. at very low volume levels) the graph becomes super coarse
+    // and 'jumpy'...so keep some minimum distance between them (this
+    // also lets the graph go to zero when no sound is playing)
+    
+    if(debug)  Serial.println("Vertically scaling the output graph");
+    
+    if(debug)
+    {
+      Serial.print("minLvlAvg : ");
+      Serial.println(minLvlAvg[x]);
+      Serial.print("maxLvlAvg : ");
+      Serial.println(maxLvlAvg[x]);
+    }
+    
+    if((maxLvl - minLvl) < 8) 
+    {
+      maxLvl = minLvl + 8;
+    }
+    
+    minLvlAvg[x] = (minLvlAvg[x] * 7 + minLvl) >> 3; // Dampen min/max levels
+    maxLvlAvg[x] = (maxLvlAvg[x] * 7 + maxLvl) >> 3; // (fake rolling average)
+    
+    // Temp swap
+    int temp_swap = 0;
+    if (maxLvlAvg[x] < minLvlAvg[x]) 
+    {
+      temp_swap    = minLvlAvg[x]; //Min = min | Max = max | Temp = min 
+      minLvlAvg[x] = maxLvlAvg[x]; //Min = max | Max = max | Temp = min
+      maxLvlAvg[x] = temp_swap;    //Min = max | Max = min | Temp = min
+    }
+
+    if(debug)  Serial.println("Fixed point scale based on dynamic min/max levels");
+    
+    // Second fixed-point scale based on dynamic min/max levels:
+    level = 10L * (col[x][colCount] - minLvlAvg[x]) / (long)(maxLvlAvg[x] - minLvlAvg[x]);
+
+    if(debug)  Serial.println("Clip the output and convert it to byte");
+    
+    // Clip output and convert to byte:
+    if(level < 0L)      c = 0;
+    else if(level > 10) c = 10; // Allow dot to go a couple pixels off top
+    else                c = (uint8_t)level;
+
+    if(debug)  Serial.println("Keep the dot on the top");
+    
+    if(c > peak[x]) peak[x] = c; // Keep dot on top
+
+    if(peak[x] <= 0) 
+    { 
+      // Empty column?
+      matrix.drawLine(x, 0, x, 7, LED_OFF);
+      continue;
+    } 
+    else if(c < 8) 
+    { 
+      // Partial column?
+      matrix.drawLine(x, 0, x, 7 - c, LED_OFF);
+    }
+
+    // The 'peak' dot color varies, but doesn't necessarily match
+    // the three screen regions...yellow has a little extra influence.
+    if(debug)  Serial.println("Draw pixels");
+    
+    y = 8 - peak[x];
+    if(y < 2)      matrix.drawPixel(x, y, LED_RED);
+    else if(y < 6) matrix.drawPixel(x, y, LED_YELLOW);
+    else           matrix.drawPixel(x, y, LED_GREEN);
+  }
+
+  matrix.writeDisplay();
+  
+  // Every third frame, make the peak pixels drop by 1:
+  if(debug)  Serial.println("Make pixel drop by 1 every 3 frame dotcount");
+  
+  if(++dotCount >= 3) 
+  {
+    dotCount = 0;
+    for(x=0; x<8; x++) 
+    {
+      if(peak[x] > 0) peak[x]--;
+    }
+  }
+  
+  if(++colCount >= 10) colCount = 0;   
 }
