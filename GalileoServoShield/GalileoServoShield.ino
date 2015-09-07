0ca3307c04f8336ce9d447deb60addc8dcd16a29
Nik Adli <adli89@live.com>
2015-09-07 22:17:25 +0800

    Updated SERVOMAX value.

--- GalileoServoShield/GalileoServoShield.ino
+++ GalileoServoShield/GalileoServoShield.ino
@@ -5,7 +5,7 @@
 Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
 
 #define SERVOMIN  100 // this is the 'minimum' pulse length count (out of 4096)
-#define SERVOMAX  488 // this is the 'maximum' pulse length count (out of 4096)
+#define SERVOMAX  560 // this is the 'maximum' pulse length count (out of 4096)
 uint8_t servonum = 0;
 uint16_t pulseLength = 0;
 uint16_t pulselength0 = 0;
@@ -29,15 +29,15 @@ void setup() {
   pwm.setPWMFreq(50);  // Analog servos run at ~60 Hz updates
 }
 
-int delayTime = 300;
+int delayTime = 1500;
 
 void loop() {
   // Drive each servo one at a time
 // ------------- FOR PRODUCTION -------------
 // for (uint8_t i=0; i<=7; i++)
 // {
-//    pulselength = map(amplitude[i], 0, 180, SERVOMIN, SERVOMAX);
-//    pwm.setPWM(i, 0, pulselength);
+//    pulseLength = map(0, 0, 180, SERVOMIN, SERVOMAX);
+//    pwm.setPWM(i, 0, pulseLength);
 // } 
 //  
 // delay(delayTime);
@@ -46,38 +46,40 @@ void loop() {
 // ------------- FOR TESTING ------------- 
   pulselength0 = map(0, 0, 180, SERVOMIN, SERVOMAX);
   pwm.setPWM(0, 0, pulselength0);
-  pulselength1 = map(30, 0, 180, SERVOMIN, SERVOMAX);
+  pulselength1 = map(0, 0, 180, SERVOMIN, SERVOMAX);
   pwm.setPWM(1, 0, pulselength1);
-   pulselength2 = map(40, 0, 180, SERVOMIN, SERVOMAX);
+  pulselength2 = map(0, 0, 180, SERVOMIN, SERVOMAX);
   pwm.setPWM(2, 0, pulselength2);
-  pulselength3 = map(90, 0, 180, SERVOMIN, SERVOMAX);
+  pulselength3 = map(0, 0, 180, SERVOMIN, SERVOMAX);
   pwm.setPWM(3, 0, pulselength3);
-   pulselength4 = map(70, 0, 180, SERVOMIN, SERVOMAX);
+  pulselength4 = map(0, 0, 180, SERVOMIN, SERVOMAX);
   pwm.setPWM(4, 0, pulselength4);
-  pulselength5 = map(90, 0, 180, SERVOMIN, SERVOMAX);
+  pulselength5 = map(0, 0, 180, SERVOMIN, SERVOMAX);
   pwm.setPWM(5, 0, pulselength5);
-   pulselength6 = map(80, 0, 180, SERVOMIN, SERVOMAX);
+  pulselength6 = map(0, 0, 180, SERVOMIN, SERVOMAX);
   pwm.setPWM(6, 0, pulselength6);
-  pulselength7 = map(9, 0, 180, SERVOMIN, SERVOMAX);
+  pulselength7 = map(0, 0, 180, SERVOMIN, SERVOMAX);
   pwm.setPWM(7, 0, pulselength7);
   
   delay(delayTime);
   
-  pulselength0 = map(90, 0, 180, SERVOMIN, SERVOMAX);
+  int endDegree = 180;
+  
+  pulselength0 = map(endDegree, 0, 180, SERVOMIN, SERVOMAX);
   pwm.setPWM(0, 0, pulselength0);
-  pulselength1 = map(90, 0, 180, SERVOMIN, SERVOMAX);
+  pulselength1 = map(endDegree, 0, 180, SERVOMIN, SERVOMAX);
   pwm.setPWM(1, 0, pulselength1);
-  pulselength2 = map(90, 0, 180, SERVOMIN, SERVOMAX);
+  pulselength2 = map(endDegree, 0, 180, SERVOMIN, SERVOMAX);
   pwm.setPWM(2, 0, pulselength2);
-  pulselength3 = map(90, 0, 180, SERVOMIN, SERVOMAX);
+  pulselength3 = map(endDegree, 0, 180, SERVOMIN, SERVOMAX);
   pwm.setPWM(3, 0, pulselength3);
-  pulselength4 = map(90, 0, 180, SERVOMIN, SERVOMAX);
+  pulselength4 = map(endDegree, 0, 180, SERVOMIN, SERVOMAX);
   pwm.setPWM(4, 0, pulselength4);
-  pulselength5 = map(90, 0, 180, SERVOMIN, SERVOMAX);
+  pulselength5 = map(endDegree, 0, 180, SERVOMIN, SERVOMAX);
   pwm.setPWM(5, 0, pulselength5);
-  pulselength6 = map(90, 0, 180, SERVOMIN, SERVOMAX);
+  pulselength6 = map(endDegree, 0, 180, SERVOMIN, SERVOMAX);
   pwm.setPWM(6, 0, pulselength6);
-  pulselength7 = map(90, 0, 180, SERVOMIN, SERVOMAX);
+  pulselength7 = map(endDegree, 0, 180, SERVOMIN, SERVOMAX);
   pwm.setPWM(7, 0, pulselength7);
 
 
--- Integration/Integration.ino
+++ Integration/Integration.ino
@@ -58,65 +58,6 @@ Port LED code from Adafruit Picollo
   // and weightings of the FFT spectrum output values to use.  Not all
   // buckets are used -- the bottom-most and several at the top are
   // either noisy or out of range or generally not good for a graph.
-//  col0data[] = {  2,  1,  // # of spectrum bins to merge, index of first
-//    111,   8 },           // Weights for each bin
-//  col1data[] = {  4,  1,  // 4 bins, starting at index 1
-//     19, 186,  38,   2 }, // Weights for 4 bins.  Got it now?
-//  col2data[] = {  5,  2,
-//     11, 156, 118,  16,   1 },
-//  col3data[] = {  8,  3,
-//      5,  55, 165, 164,  71,  18,   4,   1 },
-//  col4data[] = { 11,  5,
-//      3,  24,  89, 169, 178, 118,  54,  20,   6,   2,   1 },
-//  col5data[] = { 17,  7,
-//      2,   9,  29,  70, 125, 172, 185, 162, 118, 74,
-//     41,  21,  10,   5,   2,   1,   1 },
-//  col6data[] = { 25, 11,
-//      1,   4,  11,  25,  49,  83, 121, 156, 180, 185,
-//    174, 149, 118,  87,  60,  40,  25,  16,  10,   6,
-//      4,   2,   1,   1,   1 },
-//  col7data[] = { 37, 16,
-//      1,   2,   5,  10,  18,  30,  46,  67,  92, 118,
-//    143, 164, 179, 185, 184, 174, 158, 139, 118,  97,
-//     77,  60,  45,  34,  25,  18,  13,   9,   7,   5,
-//      3,   2,   2,   1,   1,   1,   1 },
-
-//  col0data[] = {  0, 0,  // # of spectrum bins to merge, index of first
-//    0,   0 },           // Weights for each bin
-//  col1data[] = {  1,  1,  // 4 bins, starting at index 1
-//     1, 1,  1,   1 }, // Weights for 4 bins.  Got it now?
-//  col2data[] = {  1,  1,
-//     1, 1, 1,  1,   1, 1 },
-//  col3data[] = {  1,  1,
-//      1,  1, 1, 1,  1,  1,   1,   1 },
-//  col4data[] = { 1,  1,
-//      1,  1,  1, 1, 1, 1,  1,  1,   1,   1,   1 },
-//  col5data[] = { 1,  1,
-//      1,   1,  1,  1, 1, 1, 1, 1, 1, 1,
-//      1,  1,  1,   1,   1,   1,   1 },
-//  col6data[] = { 1, 1,
-//      1,   1,  1,  1,  1,  1, 1, 1, 1, 1,
-//    1, 1, 1,  1,  1,  1,  1,  1,  1,   1,
-//      1,   1,   1,   1,   1 },
-//  col7data[] = { 0 },
-
-//       col0data[] = {  1,  1,
-//     1, 1, 1,  1,   1, 1 },
-//       col1data[] = {  1,  1,
-//     1, 1, 1,  1,   1, 1 },
-//       col2data[] = {  1,  1,
-//     1, 1, 1,  1,   1, 1 },
-//       col3data[] = {  1,  1,
-//     1, 1, 1,  1,   1, 1 },
-//       col4data[] = {  1,  1,
-//     1, 1, 1,  1,   1, 1 },
-//       col5data[] = {  1,  1,
-//     1, 1, 1,  1,   1, 1 },
-//       col6data[] = {  1,  1,
-//     1, 1, 1,  1,   1, 1 },
-//       col7data[] = {  1,  1,
-//     1, 1, 1,  1,   1, 1 },
-
   col0data[] = { 0 },
   col1data[] = { 0 },
   col2data[] = { 0 },
@@ -143,7 +84,7 @@ Port LED code from Adafruit Picollo
   Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
   
   #define SERVOMIN  100 // This is the 'minimum' pulse length count (out of 4096)
-  #define SERVOMAX  488 // This is the 'maximum' pulse length count (out of 4096)
+  #define SERVOMAX  560 // This is the 'maximum' pulse length count (out of 4096)
   uint8_t servonum = 0;
   uint16_t pulseLength = 0;
   
